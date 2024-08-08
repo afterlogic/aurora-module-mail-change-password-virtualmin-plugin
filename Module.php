da75f603ca2107d6a1a81ee24a1c6d421596c4e7
Vasil Sokolov <vasil.sokolov@gmail.com>
2024-08-08 13:41:31 +0200

    Refactored change password logic

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 
 namespace Aurora\Modules\MailChangePasswordVirtualminPlugin;
 
+use Aurora\Modules\Mail\Models\MailAccount;
+
 /**
  * Allows users to change passwords on their email accounts in Virtualmin.
  *
@@ -24,8 +26,7 @@ class Module extends \Aurora\System\Module\AbstractModule
     public function init()
     {
         $this->subscribeEvent('Mail::Account::ToResponseArray', array($this, 'onMailAccountToResponseArray'));
-        $this->subscribeEvent('Mail::ChangeAccountPassword', array($this, 'onChangeAccountPassword'));
-        $this->subscribeEvent('StandardResetPassword::ChangeAccountPassword', array($this, 'onChangeAccountPassword'));
+        $this->subscribeEvent('ChangeAccountPassword', array($this, 'onChangeAccountPassword'));
     }
 
     /**
@@ -79,9 +80,8 @@ class Module extends \Aurora\System\Module\AbstractModule
         $bPasswordChanged = false;
         $bBreakSubscriptions = false;
 
-        $oAccount = $aArguments['Account'];
-        if ($oAccount && $this->checkCanChangePassword($oAccount) && ($oAccount->getPassword() === $aArguments['CurrentPassword']
-          || isset($aArguments['SkipCurrentPasswordCheck']) && $aArguments['SkipCurrentPasswordCheck'])) {
+        $oAccount = $aArguments['Account'] instanceof MailAccount ? $aArguments['Account'] : false;
+        if ($oAccount && $this->checkCanChangePassword($oAccount) && $oAccount->getPassword() === $aArguments['CurrentPassword']) {
             $bPasswordChanged = $this->changePassword($oAccount, $aArguments['NewPassword']);
             $bBreakSubscriptions = true; // break if mail server plugin tries to change password in this account.
         }
